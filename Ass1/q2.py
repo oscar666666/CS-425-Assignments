c6a1f0098935ef39ef18dc9c7f04ae5f82583f5f
oscar666666 <oscar88feichang6@gmail.com>
2019-09-28 01:03:34 -0600

    q2 complete

---
+++ Ass1/q2.py
@@ -0,0 +1,31 @@
+from matplotlib import pyplot as plt
+import numpy as np
+from PIL import Image
+
+rawData = open("rose.raw", 'rb').read()
+imgSize = (256, 256)  # the image size
+img = Image.frombytes('L', imgSize, rawData)
+img.save("256x256.png")  # can glsive any format you like .png
+
+imgar = np.array(img)
+print(imgar.shape)
+
+#------------produce shift 2 image----------------------------
+shift2 = np.right_shift(imgar, 2)  
+shift2 = np.left_shift(imgar, 2)  
+img = Image.fromarray(shift2)
+img.save('shift2.png')
+
+
+#------------produce shift 3 image----------------------------
+shift3 = np.right_shift(imgar, 3)  
+shift3 = np.left_shift(imgar, 3)  
+img = Image.fromarray(shift3)
+img.save('shift3.png')
+
+
+#------------produce shift 4 image----------------------------
+shift4 = np.right_shift(imgar, 4)  
+shift4 = np.left_shift(imgar, 4)  
+img = Image.fromarray(shift4)
+img.save('shift4.png')
